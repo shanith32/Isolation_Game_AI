d5836ed94585b8469b161381e52696127f05bdcc
Nadika Bandara <nsbandara32@gmail.com>
2019-02-26 02:10:08 -0600

    Updates

---
+++ precache-manifest.1240bc00e501ffdb9339e982413eaaa0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63012017b6f82fbd39f3",
+    "url": "/Isolation_Game_AI/static/js/runtime~main.63012017.js"
+  },
+  {
+    "revision": "06a510ce063e85db3a49",
+    "url": "/Isolation_Game_AI/static/js/main.06a510ce.chunk.js"
+  },
+  {
+    "revision": "a207bc0e09be1599a54b",
+    "url": "/Isolation_Game_AI/static/js/2.a207bc0e.chunk.js"
+  },
+  {
+    "revision": "06a510ce063e85db3a49",
+    "url": "/Isolation_Game_AI/static/css/main.f96105fa.chunk.css"
+  },
+  {
+    "revision": "9a252a78b6994a417a1bb84231cc96ed",
+    "url": "/Isolation_Game_AI/index.html"
+  }
+];
